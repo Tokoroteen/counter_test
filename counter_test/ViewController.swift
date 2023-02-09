140d446ccc269e0526c0287fc54af60911258b5f
Tokoroteen <kenta.orbiter@gmail.com>
2023-02-09 16:39:33 +0900

    2-7

--- counter_test/ViewController.swift
+++ counter_test/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var number: Int = 0
+    var number: Double = 0.0
     //var 変数名: データの型 = 初期値
     @IBOutlet var label: UILabel!
     //@IBOutlet var 変数名: データの型!
@@ -21,10 +21,40 @@ class ViewController: UIViewController {
     
     //数値の足し算処理
     @IBAction func plus(){
-            number = number + 1
-            label.text = String(number)
+        number = number + 0.1
+        print(number)
+        label.text = String(number)
+        
+        //もしnumberが10になったら、、、
+        if number == 10 {
+            //Bの画面に遷移させるコード
+            self.performSegue(withIdentifier: "toSecond", sender: nil)
         }
-
-
+    }
+    
+    @IBAction func minus(){
+        number = number - 0.1
+        label.text = String(number)
+    }
+    
+    @IBAction func kakeru(){
+        number = number * 2.0
+        label.text = String(number)
+    }
+    
+    @IBAction func waru(){
+        number = number / 2.0
+        label.text = String(number)
+    }
+    
+    @IBAction func amari(){
+//        number = number % 3
+//        label.text = String(number)
+    }
+    
+    @IBAction func clear(){
+        number = 0
+        label.text = String(number)
+    }
 }
 
